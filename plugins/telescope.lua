d527bae84d7fd49aa3be7d0cf93453fa76f4e7f4
weakboson <weakboson@gmail.com>
2023-04-09 14:22:39 +0900

    remove unnecessary bracket

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        file_ignore_patterns = {
-          "tmp/.*",
-          "vendor/.*"
-        }
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "tmp/.*",
+        "vendor/.*"
       }
-    },
+    }
   },
 }
