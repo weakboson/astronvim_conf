74f09c1a91e4ce32970e7d1c87c3d092453fb0f5
weakboson <weakboson@gmail.com>
2023-04-27 01:39:22 +0900

    alt i で Terminal toggle

--- mappings.lua
+++ mappings.lua
@@ -23,10 +23,19 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- terminal
+    -- https://zenn.dev/convers39/articles/fd58ab494bbffb#%E3%82%B3%E3%82%A2%E6%A9%9F%E8%83%BD
+    ["<A-i>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+    -- ["<A-H>"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle horizontal terminal" },
+    -- ["<A-V>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle vertical terminal" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    -- https://zenn.dev/convers39/articles/fd58ab494bbffb#%E3%82%B3%E3%82%A2%E6%A9%9F%E8%83%BD
+    ["<A-i>"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "toggle floating terminal" },
+    -- ["<A-H>"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "toggle horizontal terminal" },
+    -- ["<A-V>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "toggle vertical terminal" },
   },
   i = {
     ["<C-j>"] = { "<Plug>(skkeleton-enable)", desc = "skkeleton enable" },
