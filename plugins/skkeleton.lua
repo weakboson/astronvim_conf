6b1e840752da8c4307010007f384a1da3d6b3892
weakboson <weakboson@gmail.com>
2023-07-29 16:16:05 +0900

    galaxy-chromebook では辞書を指定できない

--- plugins/skkeleton.lua
+++ plugins/skkeleton.lua
@@ -2,11 +2,11 @@ return {
   -- lazy = false,
   "vim-skk/skkeleton",
   dependencies = { "vim-denops/denops.vim" },
-  config = function()
-    return {
-      defaults = {
-        globalJisyo = '~/.skk/SKK-JISYO.L',
-      },
-    }
-  end,
+  -- config = function()
+  --   return {
+  --     defaults = {
+  --       globalJisyo = '~/.skk/SKK-JISYO.L',
+  --     },
+  --   }
+  -- end,
 }
